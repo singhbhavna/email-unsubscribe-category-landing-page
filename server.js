106ac646f5d68dd340913b7385a273d3b57fc671
syymza <syymza@gmail.com>
2016-11-17 12:01:29 +0000

    Added 404

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.set('port', process.env.PORT);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
 // Listen for requests
 const server = app.listen(app.get('port'), () => {
   const port = server.address().port;
