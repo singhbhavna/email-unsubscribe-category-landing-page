1d505c71427bb9690542270833a22c2092995b41
snozza <andrewcsnead@gmail.com>
2017-02-02 16:33:20 +0000

    serve static first

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.engine('.hbs', exphbs({ }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'public'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/newsletter', (req, res, next) => {
   res.render('newsletter', { name: decodeURIComponent(req.query.name) });
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
