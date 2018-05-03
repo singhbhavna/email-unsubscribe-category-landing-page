1d01cbd2078dbe47f98ac45830332ca4c229e192
richardtuft <richard.tu@ft.com>
2018-05-03 14:03:46 +0100

    adding secret squirrel  🐿 v2.8.0

---
+++ secret-squirrel.js
@@ -0,0 +1,25 @@
+module.exports = {
+	files: {
+		allow: [],
+		allowOverrides: []
+	},
+	strings: {
+		deny: [],
+		denyOverrides: [
+			't5Zpu0LstAn4PVg8rVLKF', // package-lock.json:182
+			'NUqtwOAps4mk2Zob89MWXMHjHWg9milF', // package-lock.json:262
+			'E7AdgFBVeAPVMNcKGsHMA', // package-lock.json:282
+			'pWmJj7c3GTyPhzyvbUXJQ', // package-lock.json:296
+			'PaQqUC9SRmAiSA9CCCYd4', // package-lock.json:551|601|870
+			'oTZqweIP51xaGPI4uPa56', // package-lock.json:587
+			'sAqqVW3YtEVoFQ7J0blT8', // package-lock.json:669
+			'32BBeABfUi8V60SQ5yR6G', // package-lock.json:784
+			'v/SpyMzLbtkmh45zUdaqLAaqXqzPdSrw8p4cQVO0', // package-lock.json:789
+			'/h0Plj6enJqjz1Zbq2l5WaqYnrVbwWOWMyF3F47g', // package-lock.json:799
+			'eRzhrN1WSINYCDCbrz796z37LOe3m5tmW7RQf6oBntukAG1nmovJvhnwHHRMAfeoItc1m2Hk02WER2aQ', // package-lock.json:828
+			'GqHfToR2KcsXFymXSV90hoyw3M\\+msjFuQly\\+YzIH', // package-lock.json:946
+			'cBBzwBcNDWoFxt2XEFIpQ', // package-lock.json:957
+			'6XDMfA/BTt87skxqJlO0lAh3Dker5zThcAxG6mKz' // package-lock.json:984
+		]
+	}
+};
